95a40b0ce4556d7c67bf19ee77d3d1386a72c9eb
mikeJT <jeerasaktarat@gmail.com>
2022-10-21 10:35:43 +0700

    register was added

--- src/main/java/se331/rest/util/LabMapper.java
+++ src/main/java/se331/rest/util/LabMapper.java
@@ -4,6 +4,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import se331.rest.entity.*;
+import se331.rest.security.entity.User;
+import se331.rest.security.entity.UserDTO;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,4 +25,6 @@ public interface LabMapper {
     @Mapping(target = "authorities",
             expression = "java(organizer.getUser().getAuthorities().stream().map(auth -> auth.getName().name()).collect(Collectors.toList()))")
     OrganizerAuthDTO getOrganizerAuthDTO(Organizer organizer);
+
+    UserDTO getUserDTO(User user);
 }
